10966f785d2524ed4030ca9e53d96b8a5e5bf887
manystarsapart <manystarsapart@gmail.com>
2026-09-09 08:43:27 +0800

    fix: main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
-  import Tutorial from "$lib/components/Tutorial.svelte";
+  // import Tutorial from "$lib/components/Tutorial.svelte";
 
 </script>
-<h1 class="text-center text-4xl p-4">SkySynth</h1>
+<h1 class="text-center text-7xl p-4">SkySynth</h1>
+<br>
+<a href="/player" class="flex justify-center items-center">
+  <button class="text-4xl h-20 rounded-2xl bg-gray-900 p-10 flex justify-center items-center">
+    <div>Player</div>
+  </button>
+</a>
 
 
-<Tutorial />
+<!-- <Tutorial /> -->
 
